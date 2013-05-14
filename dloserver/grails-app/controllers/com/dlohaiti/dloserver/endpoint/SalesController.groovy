472d65483124e8bc2b52133e757e53eeaf40c028
sghill <sghill.dev@gmail.com>
2013-05-14 18:56:26 -0400

    format sales controller

--- dloserver/grails-app/controllers/com/dlohaiti/dloserver/endpoint/SalesController.groovy
+++ dloserver/grails-app/controllers/com/dlohaiti/dloserver/endpoint/SalesController.groovy
@@ -5,26 +5,26 @@ import grails.converters.JSON
 
 class SalesController {
 
-    def salesService
+  def salesService
 
-    def save() {
-        log.debug "Received $params"
+  def save() {
+    log.debug "Received $params"
 
-        Sale sale
+    Sale sale
 
-        try {
-            sale = salesService.saveSale(params)
+    try {
+      sale = salesService.saveSale(params)
 
-            if (sale.hasErrors()) {
-                // TODO Better formatting of error msgs
-                log.debug(sale.errors)
-                render(status: 422, text: [msg: sale.errors] as JSON)
-            } else {
-                render(status: 201, text: [msg: "OK"] as JSON)
-            }
-        } catch (Exception e) {
-            log.error("Error saving Sale [${params.date('timestamp', 'yyyy-MM-dd hh:mm:ss z')}]: ", e)
-            render(status: 503, text: [msg: e.message] as JSON)
-        }
+      if (sale.hasErrors()) {
+        // TODO Better formatting of error msgs
+        log.debug(sale.errors)
+        render(status: 422, text: [msg: sale.errors] as JSON)
+      } else {
+        render(status: 201, text: [msg: "OK"] as JSON)
+      }
+    } catch (Exception e) {
+      log.error("Error saving Sale [${params.date('timestamp', 'yyyy-MM-dd hh:mm:ss z')}]: ", e)
+      render(status: 503, text: [msg: e.message] as JSON)
     }
+  }
 }

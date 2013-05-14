632f498754d7e61ecd2bbd3e87b7c52ffed0869b
sghill <sghill.dev@gmail.com>
2013-05-14 19:03:15 -0400

    format reading controller

--- dloserver/grails-app/controllers/com/dlohaiti/dloserver/endpoint/ReadingController.groovy
+++ dloserver/grails-app/controllers/com/dlohaiti/dloserver/endpoint/ReadingController.groovy
@@ -5,26 +5,26 @@ import grails.converters.JSON
 
 class ReadingController {
 
-    def readingsService
+  def readingsService
 
-    def save() {
-        log.debug "Received $params"
+  def save() {
+    log.debug "Received $params"
 
-        Reading reading
+    Reading reading
 
-        try {
-            reading = readingsService.saveReading(params)
+    try {
+      reading = readingsService.saveReading(params)
 
-            if (reading.hasErrors()) {
-                // TODO Better formatting of error msgs
-                log.debug(reading.errors)
-                render(status: 422, text: [msg: reading.errors] as JSON)
-            } else {
-                render(status: 201, text: [msg: "OK"] as JSON)
-            }
-        } catch (Exception e) {
-            log.error("Error saving Reading [${params.date('timestamp', 'yyyy-MM-dd hh:mm:ss z')}]: ", e)
-            render(status: 503, text: [msg: e.message] as JSON)
-        }
+      if (reading.hasErrors()) {
+        // TODO Better formatting of error msgs
+        log.debug(reading.errors)
+        render(status: 422, text: [msg: reading.errors] as JSON)
+      } else {
+        render(status: 201, text: [msg: "OK"] as JSON)
+      }
+    } catch (Exception e) {
+      log.error("Error saving Reading [${params.date('timestamp', 'yyyy-MM-dd hh:mm:ss z')}]: ", e)
+      render(status: 503, text: [msg: e.message] as JSON)
     }
+  }
 }

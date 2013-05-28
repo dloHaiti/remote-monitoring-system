fdd5e5e3bd15494fef8729e237bab164b81ca02e
sghill <sghill.dev@gmail.com>
2013-05-28 16:32:29 -0400

    receipt has total gallons and price

--- dloserver/grails-app/domain/com/dlohaiti/dloserver/Receipt.groovy
+++ dloserver/grails-app/domain/com/dlohaiti/dloserver/Receipt.groovy
@@ -3,6 +3,9 @@ package com.dlohaiti.dloserver
 class Receipt {
   Date createdDate
   Kiosk kiosk
+  Integer totalGallons
+  BigDecimal total
+  String currencyCode
 
   static hasMany = [receiptLineItems: ReceiptLineItem]
 }
--- dloserver/grails-app/services/com/dlohaiti/dloserver/ReceiptsService.groovy
+++ dloserver/grails-app/services/com/dlohaiti/dloserver/ReceiptsService.groovy
@@ -6,8 +6,8 @@ class ReceiptsService {
 
   public saveReceipt(params) {
     Date createdDate = params.date("createdDate", grailsApplication.config.dloserver.measurement.timeformat.toString())
-    Receipt receipt = new Receipt(createdDate: createdDate)
-    String currencyCode = "HTG"
+    String currencyCode = params.total.currencyCode
+    Receipt receipt = new Receipt([createdDate: createdDate, totalGallons: params.totalGallons, total: params.total.amount, currencyCode: currencyCode])
 
     params.lineItems?.each { item ->
       def lineItem = new ReceiptLineItem([sku: item.sku, quantity: item.quantity, type: item.type, price: item.price.amount, currencyCode: currencyCode])

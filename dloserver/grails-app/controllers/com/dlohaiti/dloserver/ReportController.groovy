9933e851c7243bc855e1c3b9f4120154b91f3d9e
sghill <sghill@thoughtworks.com>
2013-06-06 13:57:04 -0400

    deliveries in the chart, including negatives

--- dloserver/grails-app/controllers/com/dlohaiti/dloserver/ReportController.groovy
+++ dloserver/grails-app/controllers/com/dlohaiti/dloserver/ReportController.groovy
@@ -21,46 +21,60 @@ class ReportController {
         days.add(new LocalDate().minusDays(i))
       }
 
-      def tableHeader = ['']
-      for(day in days) {
-        tableHeader.add(day.toString('MM/dd/yy'))
-      }
-      def tableData = [tableHeader]
-      for(product in products) {
-        def row = [product.sku]
-        for(day in days) {
-          def relevantReceipts = receipts.findAll({ r -> r.isOnDate(day) })
-          def totalForSku = relevantReceipts.inject(0, { acc, val -> acc + val.totalGallonsForSku(product.sku)})
-          row.add(totalForSku)
-        }
-        tableData.add(row)
-      }
-      def deliveryRow = ['Delivery']
-      for(day in days) {
-        def positiveDeliveries = deliveries.findAll({ d -> d.isOnDate(day) && d.isOutForDelivery() })
-        def positiveDeliveryCount = positiveDeliveries.inject(0, { acc, val -> acc + val.quantity })
-        def negativeDeliveries = deliveries.findAll({ d -> d.isOnDate(day) && d.isReturned() })
-        def deliveryTotal = negativeDeliveries.inject(positiveDeliveryCount, { acc, val -> acc - val.quantity })
-        deliveryRow.add(deliveryTotal)
-      }
-      tableData.add(deliveryRow)
+      def tableData = buildTableData(days, products, receipts, deliveries)
 
-      def chartHeader = ['Date']
-      for(product in products) {
-        chartHeader.add(product.sku)
-      }
-      def chartData = [chartHeader]
-      for(LocalDate day in days) {
-        def row = [day.toString('MM/dd/yy')]
-        for(product in products) {
-          def relevantReceipts = receipts.findAll({ r -> r.isOnDate(day) })
-          def totalForSku = relevantReceipts.inject(0, { acc, val -> acc + val.totalGallonsForSku(product.sku)})
-          row.add(totalForSku)
-        }
-        chartData.add(row)
-      }
+      def chartData = buildChartData(products, days, receipts, deliveries)
 
 
       [chartData: chartData, tableData: tableData]
     }
+
+  private buildChartData(List<Product> products, ArrayList days, List<Receipt> receipts, List<Delivery> deliveries) {
+    def chartHeader = ['Date']
+    for (product in products) {
+      chartHeader.add(product.sku)
+    }
+    chartHeader.add('Delivery')
+    def chartData = [chartHeader]
+    for (LocalDate day in days) {
+      def row = [day.toString('MM/dd/yy')]
+      for (product in products) {
+        def relevantReceipts = receipts.findAll({ r -> r.isOnDate(day) })
+        def totalForSku = relevantReceipts.inject(0, { acc, val -> acc + val.totalGallonsForSku(product.sku) })
+        row.add(totalForSku)
+      }
+      def positiveDeliveries = deliveries.findAll({d -> d.isOnDate(day) && d.isOutForDelivery() }).inject(0, { acc, val -> acc + val.quantity })
+      def totalDeliveries = deliveries.findAll({d -> d.isOnDate(day) && d.isReturned() }).inject(positiveDeliveries, { acc, val -> acc - val.quantity })
+      row.add(totalDeliveries)
+      chartData.add(row)
+    }
+    return chartData
+  }
+
+  private buildTableData(ArrayList days, List<Product> products, List<Receipt> receipts, List<Delivery> deliveries) {
+    def tableHeader = ['']
+    for (day in days) {
+      tableHeader.add(day.toString('MM/dd/yy'))
+    }
+    def tableData = [tableHeader]
+    for (product in products) {
+      def row = [product.sku]
+      for (day in days) {
+        def relevantReceipts = receipts.findAll({ r -> r.isOnDate(day) })
+        def totalForSku = relevantReceipts.inject(0, { acc, val -> acc + val.totalGallonsForSku(product.sku) })
+        row.add(totalForSku)
+      }
+      tableData.add(row)
+    }
+    def deliveryRow = ['Delivery']
+    for (day in days) {
+      def positiveDeliveries = deliveries.findAll({ d -> d.isOnDate(day) && d.isOutForDelivery() })
+      def positiveDeliveryCount = positiveDeliveries.inject(0, { acc, val -> acc + val.quantity })
+      def negativeDeliveries = deliveries.findAll({ d -> d.isOnDate(day) && d.isReturned() })
+      def deliveryTotal = negativeDeliveries.inject(positiveDeliveryCount, { acc, val -> acc - val.quantity })
+      deliveryRow.add(deliveryTotal)
+    }
+    tableData.add(deliveryRow)
+    return tableData
+  }
 }

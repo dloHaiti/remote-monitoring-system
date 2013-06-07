6cb5241fddc801b653c3df09cb9499a4a55a4f80
sghill <sghill@thoughtworks.com>
2013-06-07 01:20:39 -0400

    salesqtybyday page added

--- dloserver/grails-app/controllers/com/dlohaiti/dloserver/ReportController.groovy
+++ dloserver/grails-app/controllers/com/dlohaiti/dloserver/ReportController.groovy
@@ -10,29 +10,58 @@ class ReportController {
     [kioskName: request.kioskName]
   }
 
-  def volumeByDay() {
+  def salesQuantityByDay() {
     Kiosk kiosk = Kiosk.findByName(params.kioskName)
-    Date oneWeekAgoMidnight = new LocalDate().minusWeeks(1).toDateMidnight().toDate()
-    List<Delivery> deliveries = Delivery.findAllByKioskAndTimestampGreaterThanEquals(kiosk, oneWeekAgoMidnight)
-    List<Receipt> receipts = Receipt.findAllByKioskAndCreatedDateGreaterThanEquals(kiosk, oneWeekAgoMidnight)
-    List<Reading> readings = Reading.findAllByKioskAndCreatedDateGreaterThanEquals(kiosk, oneWeekAgoMidnight)
     List<Product> products = Product.findAll()
+    List<Receipt> receipts = Receipt.findAllByKioskAndCreatedDateGreaterThanEquals(kiosk, oneWeekAgoMidnight())
 
-
-    def days = []
-    for (int i = 6; i >= 0; i--) {
-      days.add(new LocalDate().minusDays(i))
+    def tableHeader = ['']
+    for(day in previousWeek()) {
+      tableHeader.add(day.toString('MM/dd/yy'))
+    }
+    def tableData = [tableHeader]
+    for(product in products) {
+      def row = [product.sku]
+      for(day in previousWeek()) {
+        def dayReceipts = receipts.findAll({ r -> r.isOnDate(day) })
+        def lineItemsWithSku = dayReceipts.collect({ dr -> dr.receiptLineItems }).findAll({ ReceiptLineItem item -> item.sku == product.sku })
+        def skuCount = lineItemsWithSku.inject(0, { Integer acc, ReceiptLineItem val -> acc + val.quantity })
+        row.add(skuCount)
+      }
+      tableData.add(row)
     }
 
-    def tableData = buildTableData(days, products, receipts, deliveries, readings)
+    [tableData: tableData, chartData: tableData]
+  }
 
-    def chartData = buildChartData(products, days, receipts, deliveries, readings)
+  def volumeByDay() {
+    Kiosk kiosk = Kiosk.findByName(params.kioskName)
+    List<Delivery> deliveries = Delivery.findAllByKioskAndTimestampGreaterThanEquals(kiosk, oneWeekAgoMidnight())
+    List<Receipt> receipts = Receipt.findAllByKioskAndCreatedDateGreaterThanEquals(kiosk, oneWeekAgoMidnight())
+    List<Reading> readings = Reading.findAllByKioskAndCreatedDateGreaterThanEquals(kiosk, oneWeekAgoMidnight())
+    List<Product> products = Product.findAll()
+
+    def tableData = buildTableData(previousWeek(), products, receipts, deliveries, readings)
+
+    def chartData = buildChartData(products, previousWeek(), receipts, deliveries, readings)
 
 
     [chartData: chartData, tableData: tableData, skusPresent: products.size()]
   }
 
-  private buildChartData(List<Product> products, ArrayList days, List<Receipt> receipts, List<Delivery> deliveries, List<Reading> readings) {
+  private Date oneWeekAgoMidnight() {
+    return new LocalDate().minusWeeks(1).toDateMidnight().toDate()
+  }
+
+  private List<LocalDate> previousWeek() {
+    def days = []
+    for (int i = 6; i >= 0; i--) {
+      days.add(new LocalDate().minusDays(i))
+    }
+    return days
+  }
+
+  private buildChartData(List<Product> products, List<LocalDate> previousWeek, List<Receipt> receipts, List<Delivery> deliveries, List<Reading> readings) {
     def chartHeader = ['Date']
     for (product in products) {
       chartHeader.add(product.sku)
@@ -40,7 +69,7 @@ class ReportController {
     chartHeader.add('Delivery')
     chartHeader.add('Totalizer')
     def chartData = [chartHeader]
-    for (LocalDate day in days) {
+    for (LocalDate day in previousWeek) {
       def row = [day.toString('MM/dd/yy')]
       for (product in products) {
         def relevantReceipts = receipts.findAll({ r -> r.isOnDate(day) })
@@ -57,15 +86,15 @@ class ReportController {
     return chartData
   }
 
-  private buildTableData(List<LocalDate> days, List<Product> products, List<Receipt> receipts, List<Delivery> deliveries, List<Reading> readings) {
+  private buildTableData(List<LocalDate> previousWeek, List<Product> products, List<Receipt> receipts, List<Delivery> deliveries, List<Reading> readings) {
     def tableHeader = ['']
-    for (day in days) {
+    for (day in previousWeek) {
       tableHeader.add(day.toString('MM/dd/yy'))
     }
     def tableData = [tableHeader]
     for (product in products) {
       def row = [product.sku]
-      for (day in days) {
+      for (day in previousWeek) {
         def relevantReceipts = receipts.findAll({ r -> r.isOnDate(day) })
         def totalForSku = relevantReceipts.inject(0, { acc, val -> acc + val.totalGallonsForSku(product.sku) })
         row.add(totalForSku)
@@ -73,7 +102,7 @@ class ReportController {
       tableData.add(row)
     }
     def deliveryRow = ['Delivery']
-    for (day in days) {
+    for (day in previousWeek) {
       def positiveDeliveries = deliveries.findAll({ d -> d.isOnDate(day) && d.isOutForDelivery() })
       def positiveDeliveryCount = positiveDeliveries.inject(0, { acc, val -> acc + val.quantity })
       def negativeDeliveries = deliveries.findAll({ d -> d.isOnDate(day) && d.isReturned() })
@@ -83,7 +112,7 @@ class ReportController {
     tableData.add(deliveryRow)
 
     def totalRow = ['TOTAL']
-    for (LocalDate day in days) {
+    for (LocalDate day in previousWeek) {
       def sales = receipts.findAll({ r -> r.isOnDate(day) }).inject(0, { acc, val -> acc + val.totalGallons })
       def deliveryPositives = deliveries.findAll({ d -> d.isOnDate(day) && d.isOutForDelivery() }).inject(0, { acc, val -> acc + val.quantity })
       def deliveryTotal = deliveries.findAll({d -> d.isOnDate(day) && d.isReturned() }).inject(deliveryPositives, { acc, val -> acc - val.quantity })
@@ -93,7 +122,7 @@ class ReportController {
 
     def totalizerRow = ['TOTALIZER']
     Readings readingsObj = new Readings(readings: readings)
-    for (LocalDate day in days) {
+    for (LocalDate day in previousWeek) {
       def gallonsFor = readingsObj.totalizeGallonsFor(day)
       totalizerRow.add(gallonsFor)
     }
---
+++ dloserver/grails-app/views/report/salesQuantityByDay.gsp
@@ -0,0 +1,40 @@
+<%@ page import="grails.converters.JSON" contentType="text/html;charset=UTF-8" %>
+<html>
+<head>
+    <title>A Report!</title>
+</head>
+
+<body>
+<script type="text/javascript" src="https://www.google.com/jsapi"></script>
+<script type="text/javascript">
+    google.load("visualization", "1", {packages: ["corechart"]});
+    google.setOnLoadCallback(drawChart);
+    function drawChart() {
+        var data = google.visualization.arrayToDataTable(<%= chartData as JSON %>);
+
+        var options = {
+            title: 'Sales Quantity by Day',
+            vAxis: {title: 'Quantity', minValue: 0},
+            isStacked: true
+        };
+
+        var chart = new google.visualization.ColumnChart(document.getElementById('chart'));
+        chart.draw(data, options);
+    }
+</script>
+
+<div id="chart" style="width:900px;height:500px"></div>
+
+<table>
+    <tbody>
+    <g:each var="row" in="${tableData}">
+        <tr>
+            <g:each in="${0..(row.size() - 1)}">
+                <td>${row[it]}</td>
+            </g:each>
+        </tr>
+    </g:each>
+    </tbody>
+</table>
+</body>
+</html>

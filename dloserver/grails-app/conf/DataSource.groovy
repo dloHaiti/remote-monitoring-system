18bb8fc752da69226094b31cf46e510913422948
sghill <sghill@thoughtworks.com>
2013-06-17 00:54:10 -0400

    changes for mysql in production

--- dloserver/grails-app/conf/DataSource.groovy
+++ dloserver/grails-app/conf/DataSource.groovy
@@ -24,20 +24,21 @@ environments {
         }
     }
     production {
-        dataSource {
-            dbCreate = "update"
-            url = "jdbc:h2:prodDb;MVCC=TRUE;LOCK_TIMEOUT=10000"
-            pooled = true
-            properties {
-               maxActive = -1
-               minEvictableIdleTimeMillis=1800000
-               timeBetweenEvictionRunsMillis=1800000
-               numTestsPerEvictionRun=3
-               testOnBorrow=true
-               testWhileIdle=true
-               testOnReturn=true
-               validationQuery="SELECT 1"
-            }
+      dataSource {
+        dbCreate = "update"  // Uncomment to modify tables
+        url = "jdbc:mysql://localhost:3306/dlo"
+        driverClassName = "com.mysql.jdbc.Driver"
+        username = "app"
+        password = "SQ7cuYGAMVtVZh+J"
+        properties {
+          maxActive = 100
+          maxIdle = 25
+          minIdle = 5
+          initialSize = 10
+          minEvictableIdleTimeMillis = 60000
+          timeBetweenEvictionRunsMillis = 60000
+          maxWait = 10000
         }
+      }
     }
 }

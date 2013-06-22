357bc98142fff8f7820d40633e6f2854892ad876
sghill <sghill@thoughtworks.com>
2013-06-22 01:25:45 -0400

    product repository test added

---
+++ dlokiosk/src/test/java/com/dlohaiti/dlokiosk/db/ProductRepositoryTest.java
@@ -0,0 +1,33 @@
+package com.dlohaiti.dlokiosk.db;
+
+import android.content.Context;
+import com.dlohaiti.dlokiosk.Base64ImageConverter;
+import com.dlohaiti.dlokiosk.domain.Product;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricTestRunner;
+
+import java.util.List;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+@RunWith(RobolectricTestRunner.class)
+public class ProductRepositoryTest {
+    KioskDatabase db = new KioskDatabase(Robolectric.application.getApplicationContext());
+    ProductRepository repository;
+
+    @Before
+    public void setUp() {
+        Context applicationContext = Robolectric.application.getApplicationContext();
+        repository = new ProductRepository(applicationContext, db, new Base64ImageConverter(applicationContext));
+    }
+
+    @Test
+    public void shouldReturnEmptyListWhenNoProducts() {
+        List<Product> products = repository.list();
+        assertThat(products.size(), is(0));
+    }
+}

22a6b5ca959c53a052eb2773ae0153b8bf401091
sghill <sghill@thoughtworks.com>
2013-06-26 19:55:21 -0400

    added new users and directories to server-setup

--- dloserver/scripts/server-setup.sh
+++ dloserver/scripts/server-setup.sh
@@ -11,23 +11,21 @@ sudo apt-get install -y tomcat7 mysql-server python-software-properties
 
 
 # create the database
-mysql -u root -p -e 'CREATE DATABASE dlo;'
+mysql -u root -p -e 'CREATE DATABASE IF NOT EXISTS dlo;'
 
 
 # create the app user
 mysql -u root -p -e 'CREATE USER app;'
-mysql -u root -p -e "GRANT ALL PRIVILEGES ON *.* TO 'app'@'localhost' IDENTIFIED BY 'password123';"
+mysql -u root -p -e "GRANT ALL PRIVILEGES ON *.* TO 'app'@'localhost' IDENTIFIED BY '6uxlWtfyXu1BsJ8W';"
 
 
 # serve on the default HTTP port
-iptables -A INPUT -i eth0 -p tcp --dport 80 -j ACCEPT
-iptables -A INPUT -i eth0 -p tcp --dport 8080 -j ACCEPT
-iptables -A PREROUTING -t nat -i eth0 -p tcp --dport 80 -j REDIRECT --to-port 8080
-# open the mysql port
-iptables -A INPUT -i eth0 -p tcp --dport 3306 -j ACCEPT
+sudo iptables -A INPUT -i eth0 -p tcp --dport 80 -j ACCEPT
+sudo iptables -A INPUT -i eth0 -p tcp --dport 8080 -j ACCEPT
+sudo iptables -A PREROUTING -t nat -i eth0 -p tcp --dport 80 -j REDIRECT --to-port 8080
 # open the ftp ports
-iptables -A INPUT -i eth0 -p tcp --dport 20 -j ACCEPT
-iptables -A INPUT -i eth0 -p tcp --dport 21 -j ACCEPT
+sudo iptables -A INPUT -i eth0 -p tcp --dport 20 -j ACCEPT
+sudo iptables -A INPUT -i eth0 -p tcp --dport 21 -j ACCEPT
 
 
 # save the iptables rules
@@ -47,16 +45,26 @@ sudo apt-get update -y
 sudo apt-get install -y vsftpd
 
 # create user for sensors
-useradd autosensor
-passwd autosensor
+sudo useradd autosensor
+sudo passwd autosensor
+sudo mkdir /home/autosensor
 
 
 # set the timezone to eastern
-dpkg-reconfigure tzdata
+sudo dpkg-reconfigure tzdata
+
+
+# app configuration
+sudo mkdir /etc/appconfig
 
 echo "************************************************************************************"
 echo "don't forget to configure vsftpd as per"
 echo "https://www.digitalocean.com/community/articles/how-to-set-up-vsftpd-on-ubuntu-12-04"
 echo "and add local_root=/tmp/incoming along with allow_writeable_chroot=YES"
 echo "then restart with `service vsftpd restart`"
+echo "************************************************************************************"
+
+echo "************************************************************************************"
+echo "don't forget to set the environment variable APP_DB_PW for database access"
+echo "and add the production datasource to /etc/appconfig/Config.groovy"
 echo "************************************************************************************"

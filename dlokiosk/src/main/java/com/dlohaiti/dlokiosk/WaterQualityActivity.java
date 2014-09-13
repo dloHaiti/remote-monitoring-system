79051bc70a7900437acf61fb69fa1e0a96f5dcef
Jijesh Mohan <jijeshmohan@gmail.com>
2014-09-13 20:35:52 +0530

    Added header view for Diagnostic list views

--- dlokiosk/src/main/java/com/dlohaiti/dlokiosk/WaterQualityActivity.java
+++ dlokiosk/src/main/java/com/dlohaiti/dlokiosk/WaterQualityActivity.java
@@ -8,7 +8,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 

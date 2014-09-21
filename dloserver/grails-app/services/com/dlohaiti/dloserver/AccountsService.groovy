d29da404ddd2684af703b2671940ef0a5539bf38
Jijesh Mohan <jijeshmohan@gmail.com>
2014-09-21 20:28:41 +0530

    Jijesh | saved accounts with saleschannels

--- dloserver/grails-app/services/com/dlohaiti/dloserver/AccountsService.groovy
+++ dloserver/grails-app/services/com/dlohaiti/dloserver/AccountsService.groovy
@@ -10,12 +10,27 @@ class AccountsService {
         if(account == null) {
             throw new Exception()
         }
-
         account.name=params.name
         account.contactName=params.contactName
         account.phoneNumber = params.phoneNumber
         account.dueAmount=new Double(params.dueAmount)
         account.customerType = CustomerType.findById(params.customerTypeId)
+
+        def salesChannels = account.channels.toArray()
+        if(salesChannels !=null ) {
+            for (sc in salesChannels) {
+                account.removeFromChannels(sc)
+            }
+        }
+        for(sc in params.channels) {
+
+          def channel = SalesChannel.get(sc.id)
+            if(channel == null) {
+                log.debug "Unable to get sales channel"
+                throw new MissingSalesChannelException()
+            }
+          account.addToChannels(channel)
+        }
         account.save(flush: true)
         return account
     }
